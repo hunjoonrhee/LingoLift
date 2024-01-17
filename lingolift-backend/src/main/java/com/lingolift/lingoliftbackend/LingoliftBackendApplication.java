f02f22991ac5624f48a28ba04f76ebba42b2889a
Hunjoon Rhee <hunjoonrhee90@gmail.com>
2024-01-17 22:31:27 +0100

    backend initial commit

---
+++ lingolift-backend/src/main/java/com/lingolift/lingoliftbackend/LingoliftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lingolift.lingoliftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoliftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoliftBackendApplication.class, args);
+    }
+
+}
---
+++ lingolift-backend/src/test/java/com/lingolift/lingoliftbackend/LingoliftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingolift.lingoliftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoliftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
